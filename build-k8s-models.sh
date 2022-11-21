aefad72730c04ae8064f0bb912f46822bb9b26e1
Josh Karpel <josh.karpel@gmail.com>
2022-11-20 22:20:15 -0600

    better models

--- build-k8s-models.sh
+++ build-k8s-models.sh
@@ -12,4 +12,15 @@ git clone --depth=1 --branch release-1.25 https://github.com/kubernetes/kubernet
 
 rm -rf "${target}"
 
-datamodel-codegen --input "${dir}/api/openapi-spec/v3" --output="${target}"
+datamodel-codegen --input "${dir}/api/openapi-spec/v3" --output="${target}" \
+  --disable-timestamp \
+  --target-python-version 3.9 \
+  --use-standard-collections \
+  --use_non_positive_negative_number_constrained_types \
+  --strict-nullable \
+  --strip-default-none \
+  --use-schema-description
+
+find kludge/_kube -type d -exec touch {}/__init__.py \;
+
+git add kludge/_kube
