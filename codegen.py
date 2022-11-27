0f0324a89d413447bb5d8a669f180f7a2a41c4c7
Josh Karpel <josh.karpel@gmail.com>
2022-11-26 21:28:52 -0600

    working on generating classes

--- codegen.py
+++ codegen.py
@@ -6,7 +6,7 @@ from tempfile import gettempdir
 from textwrap import dedent
 from typing import Iterable
 
-from openapi_schema_pydantic import OpenAPI, PathItem, Reference
+from openapi_schema_pydantic import OpenAPI, PathItem, Reference, Schema
 from rich.console import Console
 from rich.style import Style
 from rich.syntax import Syntax
@@ -36,7 +36,7 @@ def generate(
                 style=Style(color="yellow"),
             )
         )
-        r = subprocess.run(
+        subprocess.run(
             # clone --depth=1 --branch release-1.25 https://github.com/kubernetes/kubernetes "${dir}"
             ["git", "clone", "--depth", "1", "--branch", tag, KUBERNETES_REPO, str(kubernetes_dir)],
             check=True,
@@ -50,7 +50,10 @@ def generate(
 
     chunks.extend(
         [
+            "from __future__ import annotations",
             "from aiohttp import ClientSession",
+            "from pydantic import BaseModel, Field",
+            "",
         ]
     )
 
@@ -62,36 +65,29 @@ def generate(
     for path, spec in api.paths.items():
         if "watch" in path:
             continue
-        chunks.extend(generate_functions(path, spec))
+        # chunks.extend(generate_functions(path, spec))
+
+    for name, schema in api.components.schemas.items():
+        if name not in ["io.k8s.api.core.v1.Pod", "io.k8s.api.core.v1.PodSpec"]:
+            continue
+        chunks.append(generate_class(name, schema))
 
     code = "\n".join(c for c in chunks if c)
-
     console.print(Syntax(code, lexer="python"))
 
 
 RE_PARAMS = re.compile(r"\{(\w+)\}")
 
 
-def path_to_name(path: str) -> tuple[str, list[str]]:
-    params = RE_PARAMS.findall(path)
-    name = (
-        path.removeprefix("/api/")
-        .replace("/", "_")
-        .replace("namespaces_{namespace}", "namespaced")
-        .replace("{name}", "")
-        .replace("{path}", "")
-        .replace("__", "_")
-        .rstrip("_")
-    )
-
-    return name, params
+def params_from_path(path: str) -> list[str]:
+    return RE_PARAMS.findall(path)
 
 
 def object_ref_to_name(ref: str) -> str:
-    #     #/components/schemas/io.k8s.api.core.v1.ComponentStatus'
     return (
-        ref.replace("#/components/schemas/io.k8s.api.core.", "Core")
-        .replace("#/components/schemas/io.k8s.apimachinery.pkg.apis.meta.", "Core")
+        ref.replace("#/components/schemas/", "")
+        .replace("io.k8s.api.core.", "Core")
+        .replace("io.k8s.apimachinery.pkg.apis.meta.", "Core")
         .replace("v1", "V1")
         .replace(".", "")
     )
@@ -104,8 +100,7 @@ def camel_to_snake(s: str) -> str:
 
 
 def generate_functions(path: str, spec: PathItem) -> Iterable[str]:
-    print(path)
-    name, params = path_to_name(path)
+    params = params_from_path(path)
     args = ["session: ClientSession"] + [f"{p}: str" for p in params]
     fmt_args = ", ".join(args)
     if spec.get is not None:
@@ -114,6 +109,7 @@ def generate_functions(path: str, spec: PathItem) -> Iterable[str]:
         if "application/json" in response_types:
             return_type = response_types["application/json"]
             if isinstance(return_type.media_type_schema, Reference):
+                print(return_type.media_type_schema.ref)
                 rt = object_ref_to_name(return_type.media_type_schema.ref)
             else:
                 rt = return_type.media_type_schema.type
@@ -130,13 +126,60 @@ def generate_functions(path: str, spec: PathItem) -> Iterable[str]:
                     \"\"\"
                     Original path: {path}
                     Op ID: {op_id}
-                    Derived name: {name}
                     Derived params: {params}
                     \"\"\"
                     async with session.get(f"{path}", ssl=sslcontext) as response:
                         {return_line}
                 """
             )
+
+
+def generate_class(name: str, schema: Schema) -> str:
+    # console.print(name)
+    # console.print(schema)
+
+    fields = []
+
+    for prop_name, prop in schema.properties.items():
+        if prop.type is None:
+            t = object_ref_to_name(prop.allOf[0].ref)
+        elif prop.type == "string":
+            t = "str"
+        elif prop.type == "integer":
+            t = "int"
+        elif prop.type == "boolean":
+            t = "bool"
+        elif prop.type == "array":
+            if isinstance(prop.items, Reference):
+                item_type = object_ref_to_name(prop.items.ref)
+            else:
+                item_type = "foo"
+            t = f"list[{item_type}]"
+        else:
+            print(f"panic {prop.type=}")
+
+        if prop.default is not None:
+            if isinstance(prop.default, dict) and prop.type is None:
+                d = f"default_factory={t}"
+            else:
+                d = f"default={prop.default}"
+        else:
+            d = "..."
+
+        fields.append(f'{camel_to_snake(prop_name)}: {t} = Field({d}, alias="{prop_name}")')
+        # fields.append(f"{camel_to_snake(prop_name)}: {t} = Field({d}, alias=\"{prop_name}\", description=\"{prop.description}\")")
+
+    fmt_fields = f"\n{' ' * 12}".join(fields)
+
+    return dedent(
+        f"""\
+        class {object_ref_to_name(name)}(BaseModel):
+            \"\"\"
+            {schema.description}
+            \"\"\"
+            {fmt_fields}
+        """
+    )
 
 
 if __name__ == "__main__":
